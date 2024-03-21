f7fbc4a0fc9e5869d95626ec14dedfc9941455fe
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-03-21 14:47:09 +0700

    Initial Commit

---
+++ Udacity_OnTheMap/Udacity_OnTheMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udacity_OnTheMapApp.swift
+//  Udacity_OnTheMap
+//
+//  Created by Work on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Udacity_OnTheMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
